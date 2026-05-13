fde5745018ee76dc84c62131fc40876cd6dc24df
chinqan <chinqan@gmail.com>
2026-05-13 22:07:43 +0800

    Update ecosystem.config.cjs

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: 'gem4',
       script: 'npx',
-      args: 'vite preview --host 0.0.0.0 --port 7860',
+      args: 'vite preview --host 0.0.0.0 --port 8188',
       cwd: './',
       env: {
         NODE_ENV: 'production'
